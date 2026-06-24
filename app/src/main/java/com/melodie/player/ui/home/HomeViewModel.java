e56cdea8604ccb5c61f5b0b18a66dae2c4c82964
nbillaud <nicolas.billaud@ingenico.com>
2026-06-24 13:25:13 +0200

    Suppression du fragment home

--- app/src/main/java/com/melodie/player/ui/home/HomeViewModel.java
+++ app/src/main/java/com/melodie/player/ui/home/HomeViewModel.java
@@ -1,34 +1,7 @@
 package com.melodie.player.ui.home;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.melodie.player.data.entity.Album;
-import com.melodie.player.data.entity.Song;
-import com.melodie.player.data.repository.MusicRepository;
-
-import java.util.List;
-
-import javax.inject.Inject;
-
-import dagger.hilt.android.lifecycle.HiltViewModel;
-
-@HiltViewModel
-public class HomeViewModel extends ViewModel {
-
-    private final MusicRepository repository;
-
-    @Inject
-    public HomeViewModel(MusicRepository repository) {
-        this.repository = repository;
-    }
-
-    public LiveData<List<Album>> recentAlbums() {
-        return repository.observeRecentAlbums(10);
-    }
-
-    public LiveData<List<Song>> favorites() {
-        return repository.observeFavorites();
-    }
-}
-
+/** @deprecated Removed from navigation. Safe to delete this file. */
+@Deprecated
+public class HomeViewModel extends ViewModel { }
